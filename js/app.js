b418e2764fd1eaf7d06bdd2fd5db413041663c53
Dan Riti <dmriti@gmail.com>
2013-01-15 22:24:25 -0500

    Add Tweet model, collection, and view.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,57 @@
-var app = app || {};
-
 $(function() {
 
-	// Kick things off by creating the **App**.
-	new app.AppView();
+    // Models
+    var Tweet = Backbone.Model.extend();
+
+    // Collections
+    var Tweets = Backbone.Collection.extend({
+        model: Tweet,
+        url: 'http://search.twitter.com/search.json?q=Providence&rpp=5&callback=?',
+        parse: function(response) {
+            console.log('parsing...');
+            return response.results
+        }
+    });
+
+    // Views
+    var TestView = Backbone.View.extend({
+        el: '.page',
+        render: function() {
+            this.$el.html('Foo bar');
+        },
+    });
+
+    var TweetView = Backbone.View.extend({
+        el: 'tweets',
+        initialize: function() {
+            this.tweets = new Tweets();
+            this.tweets.bind('reset', function(collection) {
+                collection.each(function(tweet) {
+                    console.log(tweet.get('text'));
+                });
+            });
+            this.tweets.fetch();
+        }
+    });
+
+    // Router
+    var Router = Backbone.Router.extend({
+        routes: {
+            '' : 'home'
+        }
+    });
+
+    // Instantiations.
+    var testView = new TestView();
+    var tweetView = new TweetView();
+    var router = new Router();
+
+    router.on('route:home', function() {
+        console.log('Load the home page!');
+        testView.render();
+    });
+
+    // Let's get this party started!
+    Backbone.history.start();
 
 });
